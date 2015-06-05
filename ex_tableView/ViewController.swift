b146357df569f76c39a88c8ba161fb6eedca47ef
Naoyuki Sugi <is0218hv@ed.ritsumei.ac.jp>
2015-06-06 03:20:46 +0900

    Initial Commit

---
+++ ex_tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ex_tableView
+//
+//  Created by Naoyuki Sugi on 2015/06/06.
+//  Copyright (c) 2015年 Naoyuki Sugi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
